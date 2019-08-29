c72c132d05394e9e5ace007b11ccbc3f51a12fe9
rwebster <r.webster.2@research.gla.ac.uk>
2019-08-29 22:42:11 +0100

    Include a setup.py file

---
+++ setup.py
@@ -0,0 +1,49 @@
+from setuptools import setup
+import codecs
+import os
+import re
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+# parse version from init
+# from: https://github.com/pypa/pip/blob/master/setup.py
+here = os.path.abspath(os.path.dirname(__file__))
+
+
+def read(*parts):
+    # intentionally *not* adding an encoding option to open, See:
+    #   https://github.com/pypa/virtualenv/issues/201#issuecomment-3145690
+    with codecs.open(os.path.join(here, *parts), 'r') as fp:
+        return fp.read()
+
+
+def find_version(*file_paths):
+    version_file = read(*file_paths)
+    version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]", version_file, re.M)
+    if version_match:
+        return version_match.group(1)
+    raise RuntimeError("Unable to find version string.")
+
+    setup(name='SQuEELS',
+        version=find_version("SQuEELS", "__init__.py"),
+        description="Standard-based Quantification of EELS (SQuEELS) data.",
+        classifiers=[
+        'Programming Language :: Python :: 3',
+        'Topic :: Scientific/Engineering :: Physics',
+        ],
+        keywords=[
+        'microscopy',
+        'STEM',
+        'TEM',
+        'EELS',
+        ],
+        url='http://gitlab.com/rwebster/SQuEELS',
+        author='Robert Webster',
+        author_email='rwhwebster@gmail.com',
+        packages=['SQuEELS'],
+        install_requires=[
+        'numpy',
+        'scipy',
+        'hyperspy',
+        ],
+        zip_safe=True)
