fd248d4486dc99139e246657d961f264d42f1556
rwebster <r.webster.2@research.gla.ac.uk>
2019-09-04 13:54:26 +0100

    Add tests for remedy_quadrant_glitch

---
+++ SQuEELS/tests/test_processing.py
@@ -0,0 +1,29 @@
+from __future__ import print_function
+import unittest
+import numpy as np
+
+import SQuEELS
+import SQuEELS.processing as sqp
+
+class TestQuantify(unittest.TestCase):
+    def test_quad_glitch_cor_caseA(self):
+        nom_corr = np.ones(2048)
+        glitch = np.ones(2048)
+        glitch[:1024] += 0.1
+        glitch[1024:] -= 0.1
+        corr = sqp.remedy_quadrant_glitch(glitch, gc=1024, width=10)
+
+        assert np.allclose(corr, nom_corr)
+
+    def test_quad_glitch_cor_caseB(self):
+        nom_corr = np.ones(2048)
+        glitch = np.ones(2048)
+        glitch[:1024] -= 0.1
+        glitch[1024:] += 0.1
+        corr = sqp.remedy_quadrant_glitch(glitch, gc=1024, width=10)
+
+        assert np.allclose(corr, nom_corr)
+
+
+if __name__ == '__main__':
+    unittest.main()
