4d3debee12dbdaf73b6ec1238e6be37db0ea9d45
Robert Webster <r.webster.2@research.gla.ac.uk>
2019-12-16 23:52:24 +0000

    Added new tests for Data class and methods

--- SQuEELS/tests/test_io.py
+++ SQuEELS/tests/test_io.py
@@ -89,6 +89,84 @@ class TestStandards(unittest.TestCase):
         shutil.rmtree(self.tmp_dir)
 
 
+class TestData(unittest.TestCase):
+    @classmethod
+    def setUpClass(self):
+        self.tmp_dir = tempfile.mkdtemp(prefix='sq-')
+        self.bp = os.path.dirname(__file__)
+
+        fn = os.path.join(self.bp, 'test_data', 'test_data.zip')
+        with zipfile.ZipFile(fn, 'r') as z:
+            z.extractall(path=self.tmp_dir)
+            fns = z.namelist()
+        self.fns = [x for x in fns if x.split('.')[-1].lower() in ['dm3']]
+        self.fns.sort()
+
+    def test_init_basic(self):
+        data = sqio.Data(fp=os.path.join(self.tmp_dir, 'high_loss.dm3'), DEELS=False)
+
+    def test_init_w_low_loss(self):
+        data = sqio.Data(fp=os.path.join(self.tmp_dir, 'high_loss.dm3'),
+            DEELS=True,
+            LL=os.path.join(self.tmp_dir, 'low_loss.dm3'))
+
+    def test_project_signal_on_axis_1(self):
+        # Case 1, projection along y axis
+        data = sqio.Data(fp=os.path.join(self.tmp_dir, 'high_loss.dm3'), DEELS=False)
+        dims = list(data.data.axes_manager.shape)
+        dims[0] = 1
+        data.project_signal_on_axis(0)
+        assert dims == list(data.data.axes_manager.shape)
+
+    def test_project_signal_on_axis_2(self):
+        # Case 2, projection along y axis
+        data = sqio.Data(fp=os.path.join(self.tmp_dir, 'high_loss.dm3'), DEELS=False)
+        dims = list(data.data.axes_manager.shape)
+        dims[1] = 1
+        data.project_signal_on_axis(1)
+        assert dims == list(data.data.axes_manager.shape)
+
+    def test_project_signal_on_axis_3(self):
+        # Case three, projection including low-loss data
+        data = sqio.Data(fp=os.path.join(self.tmp_dir, 'high_loss.dm3'),
+            DEELS=True,
+            LL=os.path.join(self.tmp_dir, 'low_loss.dm3'))
+        dims = list(data.data.axes_manager.shape)
+        dims[1] = 1
+        dims_LL = list(data.LL.axes_manager.shape)
+        dims_LL[1] = 1
+        data.project_signal_on_axis(1)
+        assert dims == list(data.data.axes_manager.shape)
+        assert dims_LL == list(data.LL.axes_manager.shape)
+
+    def test_rebin_energy_1(self):
+        data = sqio.Data(fp=os.path.join(self.tmp_dir, 'high_loss.dm3'), DEELS=False)
+        dims = list(data.data.axes_manager.shape)
+        dims[2] /= 2
+        data.rebin_energy(2)
+        assert dims == list(data.data.axes_manager.shape)
+
+    def test_rebin_energy_2(self):
+        data = sqio.Data(fp=os.path.join(self.tmp_dir, 'high_loss.dm3'),
+            DEELS=True,
+            LL=os.path.join(self.tmp_dir, 'low_loss.dm3'))
+        dims = list(data.data.axes_manager.shape)
+        dims[2] /= 2
+        dims_LL = list(data.LL.axes_manager.shape)
+        dims_LL[2] /= 2
+        data.rebin_energy(2)
+        assert dims == list(data.data.axes_manager.shape)
+        assert dims_LL == list(data.LL.axes_manager.shape)
+
+    def test_deconvolve_hook(self):
+        data = sqio.Data(fp=os.path.join(self.tmp_dir, 'high_loss.dm3'),
+            DEELS=True,
+            LL=os.path.join(self.tmp_dir, 'low_loss.dm3'))
+        data.deconvolve()
+
+    @classmethod
+    def tearDownClass(self):
+        shutil.rmtree(self.tmp_dir)
 
 if __name__ == '__main__':
     unittest.main()
