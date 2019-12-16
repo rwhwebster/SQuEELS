01fdfee25cb86fcb4099b0a993055c2ed386df08
rwebster <r.webster.2@research.gla.ac.uk>
2019-12-16 13:15:22 +0000

    Update utils tests to fix generation of hyperspy signals

--- SQuEELS/tests/test_utils.py
+++ SQuEELS/tests/test_utils.py
@@ -18,33 +18,84 @@ class TestUtils(unittest.TestCase):
 
     def test_pad_spectrum(self):
         # Mock spectrum object
-        spectrum = hs.signals.Signal1D(2048)
+        spectrum = hs.signals.Signal1D(np.zeros(2048))
         spectrum.data[:] = 1.0
         # Pass mock spectrum through pad_spectrum
         new_spectrum = squ.pad_spectrum(spectrum, 4096)
 
+    def test_zero_pad_spectrum_1d(self):
+        # Mock spectrum object
+        spectrum = hs.signals.Signal1D(np.zeros(2048))
+        spectrum.data[:] = 1.0
+        # Pass mock spectrum through pad_spectrum
+        axis = spectrum.axes_manager.signal_indices_in_array[0]
+        new_spectrum = squ.zero_pad_spectrum(spectrum, 4096, axis)
+        assert np.allclose(new_spectrum.data.shape, (4096,))
+
+    def test_zero_pad_spectrum_2d(self):
+        # Mock spectrum object
+        spectrum = hs.signals.Signal1D(np.zeros((10,2048)))
+        spectrum.data[:] = 1.0
+        # Pass mock spectrum through pad_spectrum
+        axis = spectrum.axes_manager.signal_indices_in_array[0]
+        new_spectrum = squ.zero_pad_spectrum(spectrum, 4096, axis)
+        assert np.allclose(new_spectrum.data.shape, (10, 4096))
+
+    def test_zero_pad_spectrum_3d(self):
+        # Mock spectrum object
+        spectrum = hs.signals.Signal1D(np.zeros((10,10,2048)))
+        spectrum.data[:] = 1.0
+        # Pass mock spectrum through pad_spectrum
+        axis = spectrum.axes_manager.signal_indices_in_array[0]
+        new_spectrum = squ.zero_pad_spectrum(spectrum, 4096, axis)
+        assert np.allclose(new_spectrum.data.shape, (10, 10, 4096))
+
     def test_match_spectra_sizes_1d(self):
-        s1 = hs.signals.Signal1D(2000)
+        s1 = hs.signals.Signal1D(np.zeros(2000))
         s1.data[:] = 1.0
-        s2 = hs.signals.Signal1D(1000)
+        s2 = hs.signals.Signal1D(np.zeros(1000))
         s2.data[:] = 1.0
-        o1, o2 = squ.match_spectra_sizes(s1, s2)
+        o1, o2 = squ.match_spectra_sizes(s1, s2, taper=False)
         assert np.allclose(o1.data.shape, o2.data.shape)
 
     def test_match_spectra_sizes_2d(self):
-        s1 = hs.signals.Signal1D((10,2000))
+        s1 = hs.signals.Signal1D(np.zeros((10,2000)))
         s1.data[:] = 1.0
-        s2 = hs.signals.Signal1D((10,1000))
+        s2 = hs.signals.Signal1D(np.zeros((10,1000)))
         s2.data[:] = 1.0
-        o1, o2 = squ.match_spectra_sizes(s1, s2)
+        o1, o2 = squ.match_spectra_sizes(s1, s2, taper=False)
         assert np.allclose(o1.data.shape, o2.data.shape)
 
     def test_match_spectra_sizes_3d(self):
-        s1 = hs.signals.Signal1D((10,20,2000))
+        s1 = hs.signals.Signal1D(np.zeros((10,20,2000)))
         s1.data[:] = 1.0
-        s2 = hs.signals.Signal1D((10,20,1000))
+        s2 = hs.signals.Signal1D(np.zeros((10,20,1000)))
         s2.data[:] = 1.0
-        o1, o2 = squ.match_spectra_sizes(s1, s2)
+        o1, o2 = squ.match_spectra_sizes(s1, s2, taper=False)
+        assert np.allclose(o1.data.shape, o2.data.shape)
+
+    def test_match_spectra_sizes_1d_with_taper(self):
+        s1 = hs.signals.Signal1D(np.zeros(2000))
+        s1.data[:] = 1.0
+        s2 = hs.signals.Signal1D(np.zeros(1000))
+        s2.data[:] = 1.0
+        o1, o2 = squ.match_spectra_sizes(s1, s2, taper=True, size=100)
+        assert np.allclose(o1.data.shape, o2.data.shape)
+
+    def test_match_spectra_sizes_2d_with_taper(self):
+        s1 = hs.signals.Signal1D(np.zeros((10,2000)))
+        s1.data[:] = 1.0
+        s2 = hs.signals.Signal1D(np.zeros((10,1000)))
+        s2.data[:] = 1.0
+        o1, o2 = squ.match_spectra_sizes(s1, s2, taper=True, size=100)
+        assert np.allclose(o1.data.shape, o2.data.shape)
+
+    def test_match_spectra_sizes_3d_with_taper(self):
+        s1 = hs.signals.Signal1D(np.zeros((10,20,2000)))
+        s1.data[:] = 1.0
+        s2 = hs.signals.Signal1D(np.zeros((10,20,1000)))
+        s2.data[:] = 1.0
+        o1, o2 = squ.match_spectra_sizes(s1, s2, taper=True, size=100)
         assert np.allclose(o1.data.shape, o2.data.shape)
 
 
