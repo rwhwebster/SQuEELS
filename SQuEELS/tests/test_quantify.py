7ecf7be6eed89a97e7b3ab381b9845d41bc5dd1a
Robert Webster <r.webster.2@research.gla.ac.uk>
2019-12-17 00:44:34 +0000

    Started adding tests for MLLSmodel class

--- SQuEELS/tests/test_quantify.py
+++ SQuEELS/tests/test_quantify.py
@@ -1,10 +1,37 @@
 from __future__ import print_function
 import unittest
+import os
+import zipfile
+import tempfile
+import shutil
 import numpy as np
 
 import SQuEELS.quantify as sqq
+import SQuEELS.io as sqio
 
 class TestQuantify(unittest.TestCase):
+    @classmethod
+    def setUpClass(self):
+        self.ref_dir = tempfile.mkdtemp(prefix='sq-')
+        self.ref_bp = os.path.dirname(__file__)
+
+        ref_fn = os.path.join(self.ref_bp, 'test_data', 'ref_data.zip')
+        with zipfile.ZipFile(ref_fn, 'r') as z:
+            z.extractall(path=self.ref_dir)
+            ref_fns = z.namelist()
+        self.ref_fns = [x for x in ref_fns if x.split('.')[-1].lower() in ['dm3']]
+        self.ref_fns.sort()
+
+        self.dat_dir = tempfile.mkdtemp(prefix='sq-')
+        self.dat_bp = os.path.dirname(__file__)
+
+        dat_fn = os.path.join(self.dat_bp, 'test_data', 'test_data.zip')
+        with zipfile.ZipFile(dat_fn, 'r') as z:
+            z.extractall(path=self.dat_dir)
+            dat_fns = z.namelist()
+        self.dat_fns = [x for x in dat_fns if x.split('.')[-1].lower() in ['dm3']]
+        self.dat_fns.sort()
+
     def test_normalEqn_univariate(self):
         nDat = 10
         nom_theta = np.array([0.5, 2.0])
@@ -30,6 +57,18 @@ class TestQuantify(unittest.TestCase):
 
         assert np.allclose(theta, nom_theta)
 
+    def test_MLLSmodel_creation(self):
+        lib = sqio.Standards(fp=self.ref_dir)
+        SI = sqio.Data(fp=os.path.join(self.dat_dir, 'deconv.dm3'))
+        model = sqq.MLLSmodel(SI, lib)
+
+    def test_MLLSmodel_point(self):
+        lib = sqio.Standards(fp=self.ref_dir)
+        SI = sqio.Data(fp=os.path.join(self.dat_dir, 'deconv.dm3'))
+        SI.set_data_range(400.0, 500.0)
+        tofit = ['Ti',]
+        lib.set_spectrum_range(400.0, 500.0)
+        model = sqq.MLLSmodel(SI, lib)
 
 
 if __name__ == '__main__':
